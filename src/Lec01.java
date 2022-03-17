475db838f3d18292de95bbbe0cb84039a59dff83
KKJ <39583312+kkjsw17@users.noreply.github.com>
2022-03-17 09:33:58 +0900

    feat : data folder의 html 파일들을 xml로 변환하는 기능 구현
    각 html의 body에 있는 p 태그의 내용을 종합하여 xml에 기술, html 개수를 반영하여 xml 태그 생성

---
+++ src/Lec01.java
@@ -0,0 +1,67 @@
+package lec01;
+
+import org.jsoup.Jsoup;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+
+public class Lec01 {
+	public static void main(String args[]) throws ParserConfigurationException, IOException, TransformerException {
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		
+		Document document = docBuilder.newDocument();
+		
+		Element docs = document.createElement("docs");
+		document.appendChild(docs);
+		
+		String path = "./src/data/html";
+		File[] files = makeFileList(path);
+		                          
+		for (int i = 0; i < files.length; i++) {
+			org.jsoup.nodes.Document html = Jsoup.parse(files[i], "UTF-8");
+			String titleData = html.title();
+			String bodyData = html.body().text();
+			
+			Element doc = document.createElement("doc");
+			doc.setAttribute("id", Integer.toString(i));
+			
+			Element title = document.createElement("title");
+			title.appendChild(document.createTextNode(titleData));
+			doc.appendChild(title);
+			
+			Element body = document.createElement("body");
+			body.appendChild(document.createTextNode(bodyData));
+			doc.appendChild(body);
+			
+			docs.appendChild(doc);
+		}
+		
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		Transformer transformer = transformerFactory.newTransformer();
+		transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+		
+		DOMSource source = new DOMSource(document);
+		StreamResult result = new StreamResult(new FileOutputStream(new File("src/data/file.xml")));
+		
+		transformer.transform(source, result);
+	}
+	
+	public static File[] makeFileList(String path) {
+		File dir = new File(path);
+		return dir.listFiles();
+	}
+}
